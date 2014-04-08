3423f0947afcc253a9b2911e3dfadab8a70827c7
ivan <ivan@futureworkz.com>
2014-04-08 15:11:13 +0700

    Added homepage for easy testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Ocd::Application.routes.draw do
   devise_for :users
 
-  root :to => redirect("/users/sign_up")
+  root :to => 'application#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
