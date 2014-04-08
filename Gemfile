7209a8e8158b5873ad4da2f159b689ab3f47b2ce
ivan <ivan@futureworkz.com>
2014-04-08 14:32:52 +0700

    Added Devise gem

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 1.2'
 
 gem 'pg'
+gem 'devise'
 
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Ocd::Application.routes.draw do
+  devise_for :users
+
+  root :to => redirect("/users/sign_up")
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
