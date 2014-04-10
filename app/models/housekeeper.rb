d7472dd539536013a042b816a31e437cd086c6ee
ivan <ivan@futureworkz.com>
2014-04-10 10:30:34 +0700

    Took out before_message in DOB validation

--- app/models/housekeeper.rb
+++ app/models/housekeeper.rb
@@ -6,6 +6,5 @@ class Housekeeper < ActiveRecord::Base
   enumerize :gender, in: [:male, :female]
 
   validates_presence_of :name, :gender, :email, :contact, :address, :postal, :date_of_birth
-  validates_date :date_of_birth, :on_or_before => lambda { 18.years.ago },
-                 :before_message => "must be at least 18 years old"
+  validates_date :date_of_birth, :on_or_before => lambda { 18.years.ago }
 end
