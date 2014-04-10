ed256480cd3939917e65c9a317bdbddef8563a44
ivan <ivan@futureworkz.com>
2014-04-10 10:53:52 +0700

    Added DB seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Create admin login
+Admin.destroy_all
+Admin.create!(email: 'admin@example.com', password: '123123', password_confirmation: '123123')
+
+# Create locations
+north = Location.find_or_create_by!(name: 'North')
+east = Location.find_or_create_by!(name: 'East')
+Location.find_or_create_by!(name: 'West')
+Location.find_or_create_by!(name: 'Central')
+Location.find_or_create_by!(name: 'North-East')
+
+# Create sample housekeeper
+Housekeeper.destroy_all
+housekeeper = Housekeeper.create!(
+  name: 'Example Housekeeper',
+  gender: 'female',
+  email: 'example_housekeeper@example.com',
+  contact: '12345678',
+  address: 'Somewhere in Singapore',
+  postal: '652342',
+  date_of_birth: 20.years.ago
+)
+housekeeper.locations << [north, east]
