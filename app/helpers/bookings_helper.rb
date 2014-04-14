6f4d3bab145eaa047997dc2c9c2dde50c3628009
ivan <ivan@futureworkz.com>
2014-04-14 17:35:09 +0700

    Updated BookingsHelper

--- app/helpers/bookings_helper.rb
+++ app/helpers/bookings_helper.rb
@@ -15,14 +15,8 @@ module BookingsHelper
     calendar_time_slot = create_date_time(day, time)
     booked_time_slot = find_booked_time_slot(booked_time_slots, calendar_time_slot)
 
-    if booked_time_slot
-      if booked_time_slot.category.booked?
-        render partial: 'bookings/booked_slot'
-      else
-        render partial: 'bookings/blocked_slot'
-      end
-    else
-      render partial: 'bookings/available_slot', locals: { day: day, time: time, start_time: create_date_time(day, time) }
+    unless booked_time_slot
+      render partial: 'bookings/available_slot', locals: {day: day, time: time, start_time: create_date_time(day, time)}
     end
   end
 
