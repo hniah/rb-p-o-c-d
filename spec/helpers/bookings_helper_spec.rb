9e8ca0a9b60e136845d5fbc17dd9ec7de92e6bf2
ivan <ivan@futureworkz.com>
2014-04-15 08:56:24 +0700

    Updated BookingsHelperSpec and layout

--- spec/helpers/bookings_helper_spec.rb
+++ spec/helpers/bookings_helper_spec.rb
@@ -4,11 +4,6 @@ describe BookingsHelper do
   describe '#display_booked_slot_info' do
     let!(:time_slots) { build_list(:time_slot, 1) }
 
-    it 'display a message if the time slot is booked' do
-      time = DateTime.now.change(hour: 8, minute: 00)
-      helper.display_booked_slot_info(time_slots, Date.today, time).should have_css '.booked'
-    end
-
     it 'displays a button for user to book if the time slot is available' do
       time = DateTime.now.change(hour: 16, minute: 0)
       helper.display_booked_slot_info(time_slots, Date.today, time).should have_css ".available"
