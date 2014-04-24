809ee26c4f7ee8af2e07ea4bb230c49013b3dd7b
ivan <ivan@futureworkz.com>
2014-04-24 16:40:18 +0700

    Updated TimeSlotConcernsFinder

--- app/models/concerns/time_slot/finder.rb
+++ app/models/concerns/time_slot/finder.rb
@@ -4,9 +4,4 @@ module Concerns::TimeSlot::Finder
   def total_sessions_in_day(date)
     TimeSlot.where(start_time: date.beginning_of_day..date.end_of_day).count
   end
-
-  extended(TimeSlot) do
-    scope :created_after,  -> (date) { where('created_at >= ?', date) }
-    scope :created_before, -> (date) { where('created_at <= ?', date) }
-  end
 end
