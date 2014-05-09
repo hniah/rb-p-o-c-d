455a69b928d7a6869552d94e70ad67de2eb26564
ivan <ivan@futureworkz.com>
2014-05-09 16:26:56 +0700

    Updated EVN SMTP mailer

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Ocd::Application.initialize!
+
+config.action_mailer.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :address         => "smtp.sendgrid.net",
+  :port            => "587",
+  :user_name       => ENV["SENDGRID_USERNAME"],
+  :password        => ENV["SENDGRID_PASSWORD"],
+  :domain          => "heroku.com",
+  :authentication  => :plain,
+  :enable_starttls_auto => true
+}
