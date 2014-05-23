b1904ce4e881d7c51d52aa07b9a949a996dd1ba8
ivan <ivan@futureworkz.com>
2014-05-23 14:50:15 +0700

    Update view of user in backend

--- app/models/concerns/user/rails_admin_config.rb
+++ app/models/concerns/user/rails_admin_config.rb
@@ -21,9 +21,9 @@ module Concerns::User::RailsAdminConfig
           end
         end
         field :number_of_hour_used do
-          label "Total left hours"
+          label "Total available hours"
           def value
-            bindings[:object].total_hours_used
+            bindings[:object].total_hours_current
           end
         end
       end
