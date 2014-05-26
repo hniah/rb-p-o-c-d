a9e529384a3f2a78e1d80e391cb77f88cc30d5eb
ivan <ivan@futureworkz.com>
2014-05-26 13:16:21 +0700

    Update user info view

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,4 +3,8 @@ module UsersHelper
     day_check = (start_time - 1.day).change(hour: 18, min: 15)
     Time.zone.now < day_check
   end
+
+  def check_show_delete?(start_time)
+    Time.zone.now < start_time
+  end
 end
--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -20,4 +20,24 @@ describe UsersHelper do
       it { helper.refund?(time_slot.start_time).should_not be_true }
     end
   end
+
+  describe "#check_show_delete???" do
+    context "should show delete button" do
+      let(:time_slot) { create(:time_slot,
+                               start_time: time_with_zone(hour: 10, min: 0),
+                               end_time: time_with_zone(hour: 13, min: 0)
+      )}
+
+      it { helper.check_show_delete?(time_slot.start_time).should be_true }
+    end
+
+    context "should not show delete button" do
+      let(:time_slot) { create(:time_slot,
+                               start_time: time_with_zone(hour: 10, min: 0) - 2.day,
+                               end_time: time_with_zone(hour: 13, min: 0) - 2.day
+      )}
+
+      it { helper.check_show_delete?(time_slot.start_time).should_not be_true }
+    end
+  end
 end
