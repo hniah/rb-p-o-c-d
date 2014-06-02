8dfd0e27345852464ea58ece0d3203eccea8e727
ivan <ivan@futureworkz.com>
2014-06-02 13:58:06 +0700

    Update Page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
   def display_active_menu_item(page_url)
     current_page?(page_url) ? 'active' : ''
   end
+
+  def page_title(separator = " – ")
+    [content_for(:title), 'Our Cleaning Department'].compact.join(separator)
+  end
+
+  def page_heading(title)
+    content_for(:title){ title }
+    content_tag(:h1, title)
+  end
 end
