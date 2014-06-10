d07848365e6a18573eb3ab2a08176ba389cb4efe
ivan <ivan@futureworkz.com>
2014-06-10 13:27:09 +0700

    Update MailchimpApiController exception

--- app/controllers/mailchimp_api_controller.rb
+++ app/controllers/mailchimp_api_controller.rb
@@ -9,12 +9,17 @@ class MailchimpApiController < ApplicationController
                                                    email: {email: mailchimp_param['mailchimp_email']},
                                                    double_optin: false,
                                                  })
-  flash[:notice] = "Subscribe successfully!!!"
+  flash[:notice] = "Thank you for subscribing to our mailing list. We are happy to update you with any new OCD happenings. Have a great day!"
   redirect_to root_path
 
   rescue Exception => e
-    flash[:alert] = e.message
-    redirect_to root_path
+    if e.message == "The email parameter should include an email, euid, or leid key"
+      flash[:alert] = "Please provide a valid email address to join our mailing list. "
+      redirect_to root_path
+    else
+      flash[:alert] = e.message
+      redirect_to root_path
+    end
   end
 
   protected
