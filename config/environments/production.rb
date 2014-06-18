bc33716c10c4bd064ed2d4382160cd27a02c95a8
ivan <ivan@futureworkz.com>
2014-06-18 14:11:01 +0700

    change config.action_mailer.default_url_options environment/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Ocd::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: 'ourcleaningdepartment.com' }
-  config.action_controller.asset_host = "http://ourcleaningdepartment.com"
+  config.action_controller.asset_host = "http://ourcleaningdepartment.herokuapp.com"
   config.action_mailer.asset_host = config.action_controller.asset_host
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
