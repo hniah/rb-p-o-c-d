fd69e6abae8ea15bc48082cc2015d6018bcf6fbc
Jack A. Huang <jack@futureworkz.com>
2014-10-14 14:26:26 +0700

    Change Heroku mailing using Mandrill

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Ocd::Application.initialize!
 
-ActionMailer::Base.smtp_settings = {
-  :address         => "smtp.sendgrid.net",
-  :port            => "587",
-  :user_name       => ENV["SENDGRID_USERNAME"],
-  :password        => ENV["SENDGRID_PASSWORD"],
-  :domain          => "heroku.com",
-  :authentication  => :plain,
-  :enable_starttls_auto => true
-}
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,16 @@ Ocd::Application.configure do
     }
     ::EXPRESS_GATEWAY = ActiveMerchant::Billing::PaypalExpressGateway.new(paypal_options)
   end
+
+
+  # Heroku Mandrill configuration
+  ActionMailer::Base.smtp_settings = {
+      :port =>           '587',
+      :address =>        'smtp.mandrillapp.com',
+      :user_name =>      ENV['MANDRILL_USERNAME'],
+      :password =>       ENV['MANDRILL_APIKEY'],
+      :domain =>         'heroku.com',
+      :authentication => :plain
+  }
+  ActionMailer::Base.delivery_method = :smtp
 end
