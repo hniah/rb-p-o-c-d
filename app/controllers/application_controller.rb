0f5ba7a79e17e0ee9af5990c39b19dbb8a6de319
Jack A. Huang <jack@futureworkz.com>
2014-11-10 15:12:36 +0700

    Update user_after_signin_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    stored_location_for(resource) || root_path
+    user_info_path
   end
 
   protected
