0645f13982f440b437fc9edfa13daf199059ce8f
anthony <anthony@futureworkz.com>
2015-02-11 10:34:35 +0700

    Fix layout Rates page

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -2,7 +2,19 @@ class PackagesController < ApplicationController
 
   def buy_package
     @packages = Package.all_packages_in_array
+
     @sessions_type_list = Package::SESSION_TYPE_LIST
+    @packages_follow_session = []
+    @sessions_type_list.each do |session_type|
+      @index = 0
+      @packages[session_type].each do |package|
+        if @packages_follow_session[@index].nil?
+          @packages_follow_session[@index] = []
+        end
+        @packages_follow_session[@index] << package
+        @index+=1;
+      end
+    end
   end
 
 end
