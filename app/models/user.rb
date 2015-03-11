ff01d29d041b88e5e1e8d8d128ef7616049e22e9
Anthony <anthony@futureworkz.com>
2015-03-11 09:49:55 +0700

    Fix path of photo user

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,11 +23,12 @@ class User < ActiveRecord::Base
 
 
   DEFAULT_URL = '/images/users/avatars/:style/missing.jpg'
-
+  PATH = '/public/:class/:attachment/:id/:style_:basename.:extension'
   VALIDATE_SIZE = { in: 0..1.megabytes, message: 'Photo size too large. Please limit to 1 mb.' }
   has_attached_file :avatar,
                     styles: {small: '127x127#', thumb: '210x210#', large: '600x600#'},
-                    default_url: DEFAULT_URL
+                    default_url: DEFAULT_URL,
+                    path: PATH
   validates_attachment :avatar,
                        content_type: {content_type: /\Aimage\/.*\Z/},
                        size: VALIDATE_SIZE
