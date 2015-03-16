d6514ba78506ebe662525668951a36a6e5ef2497
Anthony <anthony@futureworkz.com>
2015-03-16 14:01:23 +0700

    compile assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Ocd::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
   config.assets.precompile += %w(ckeditor)
   config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
 
